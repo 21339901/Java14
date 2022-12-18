cd3a66bb5c5466476bdb2dbeaebf27b6717f5b2c
21339901 <113246766+21339901@users.noreply.github.com>
2022-12-18 23:33:52 +0800

    Update Class08.java

--- Class08/Class08.java
+++ Class08/Class08.java
@@ -4,23 +4,22 @@ import java.io.*;
 public class Class08 { 
 	
     public static void main(String[] args) throws IOException{
-    	int count = 0;
     	String str;
-    	char[] data_aaa = new char[21];
-    	char[] data_bbb = new char[128];
+    	BufferedWriter bw = new BufferedWriter(new FileWriter("D:\\ccc.txt"));
     	
         BufferedReader br = new BufferedReader(new FileReader("D:\\aaa.txt"));
-        br.read(data_aaa);
-        br.close();
-        br = new BufferedReader(new FileReader("D:\\bbb.txt"));
-        br.read(data_bbb); 
-        br.close();
-        FileWriter fw = new FileWriter("D:\\ccc.txt");
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write(data_aaa);
-        bw.newLine();
+    	while((str=br.readLine())!=null) {
+    		bw.write(str);
+    		bw.newLine();
+    	}
+    	br.close();
         
-        bw.write(data_bbb);
+        br = new BufferedReader(new FileReader("D:\\bbb.txt"));
+        while((str=br.readLine())!=null) {
+    		bw.write(str);
+    		bw.newLine();
+    	}
+        br.close();
         bw.close();
         
     } 
