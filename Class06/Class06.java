968d36af5924e4f8defa90c72b035007aee24690
21339901 <113246766+21339901@users.noreply.github.com>
2022-12-18 15:22:28 +0800

    Create Class06.java

---
+++ Class06/Class06.java
@@ -0,0 +1,19 @@
+package Java14;
+import java.io.*;
+
+public class Class06 { 
+	
+    public static void main(String[] args){
+    	try {
+    	char data[]=new char[128]; 
+        FileReader fr=new FileReader("train.txt"); 
+        int num=fr.read(data);    
+        String str=new String(data,0,num);    
+        System.out.println("Characters read= "+num);
+        System.out.println(str);
+        fr.close();
+    	}
+    	catch(IOException e) {
+    	}
+    } 
+}
